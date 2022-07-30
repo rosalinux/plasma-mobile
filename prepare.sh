a235e40185cabb2f63d95af915cef87b419bbf46
Emmanuel Goldstein <emgoldstein42@proton.me>
2022-07-30 12:57:58 +0000

    changing path in prepare

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
-tar --exclude='./.git' --exclude='./rpm' -czf plasma-ayya-5.23.5.tar.gz .
+tar --exclude='./.git' --exclude='./rpm' -czf ./rpm/plasma-ayya-5.23.5.tar.gz .
 mv plasma-ayya-5.23.5.tar.gz ./rpm/
