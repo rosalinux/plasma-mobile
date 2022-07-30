1167dadf25b3e2d4182114c62f8dea1e16a9d74e
Emmanuel Goldstein <emgoldstein42@proton.me>
2022-07-30 18:28:04 +0000

    update for build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 # !/bin/bash
 
 cd ./rpm/
+rm -rf BUILD BUILDROOT RPMS SRPMS plasma-*.tar.* *.buildlog
+cd ./../
+rsync -av --exclude='.git' --exclude='rpm' ./ ./plasma-phone-components-5.23.5
+tar  -czf ./rpm/plasma-phone-components-5.23.5.tar.gz plasma-phone-components-5.23.5
+rm -rf ./plasma-phone-components-5.23.5
+cd ./rpm/
 dnf builddep plasma-phone-components.spec
 abb build --nodeps --target=aarch64-openmandriva-linux
 cd ./../
