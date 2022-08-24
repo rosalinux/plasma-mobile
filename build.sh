662a0cc2d2679dae84528239eaff049f50d0c98f
Emmanuel Goldstein <emgoldstein42@proton.me>
2022-08-24 11:36:56 +0000

    another fix in build

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 cd ./rpm/
 rm -rf BUILD BUILDROOT RPMS SRPMS plasma-*.tar.* *.buildlog
 cd ./../
-rsync -av --exclude='.git' --exclude='rpm' ./ ./plasma-phone-components-5.25.3
-tar  -czf ./rpm/plasma-phone-components-5.25.3.tar.gz plasma-phone-components-5.25.3
-rm -rf ./plasma-phone-components-5.25.3
+rsync -av --exclude='.git' --exclude='rpm' ./ ./plasma-mobile-5.25.3
+tar  -czf ./rpm/plasma-mobile-5.25.3.tar.gz plasma-mobile-5.25.3
+rm -rf ./plasma-mobile-5.25.3
 cd ./rpm/
 dnf builddep plasma-mobile.spec
 abb build --nodeps --target=aarch64-openmandriva-linux
